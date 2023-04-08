484520723cfe45ca61df367dfa98df299c461d0a
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-08 09:29:28 +0530

    feat: adding material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
 import ReactDOM from 'react-dom/client';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
