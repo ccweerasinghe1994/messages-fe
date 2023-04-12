dec262f166ca87466b3eba29ed0b26883764be4b
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-12 07:44:28 +0530

    feat: header logic

--- src/pages/Login/Login.page.tsx
+++ src/pages/Login/Login.page.tsx
@@ -1,16 +1,5 @@
-import { Button, TextField, Typography } from '@mui/material';
-import { FC, useState, useEffect } from 'react';
-import Grid from '@mui/material/Unstable_Grid2';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import { FC, useEffect, useState } from 'react';
 
-const Item = styled(Paper)(({ theme }) => ({
-	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-	...theme.typography.body2,
-	padding: theme.spacing(1),
-	textAlign: 'center',
-	color: theme.palette.text.secondary,
-}));
 const LoginPage: FC = () => {
 	const [email, setEmail] = useState<string>('');
 	const [emailError, setEmailError] = useState<boolean>(false);
@@ -34,56 +23,7 @@ const LoginPage: FC = () => {
 		setPasswordError(checkPassword(password));
 	}, [password]);
 
-	return (
-		<Grid container justifyContent={'center'}>
-			<Grid xs={6}>
-				<Item>
-					<Typography variant="h3" gutterBottom>
-						Sign Up To MESSAGES
-					</Typography>
-					<TextField
-						fullWidth
-						required
-						id="filled-basic"
-						label="email"
-						variant="filled"
-						type="email"
-						value={email}
-						placeholder="sample@abc.com"
-						onChange={(e) => setEmail(e.target.value)}
-						error={!emailError}
-						helperText={!emailError ? 'Invalid email' : ''}
-						sx={{ mt: 3, mb: 2 }}
-					/>
-					<TextField
-						fullWidth
-						required
-						id="filled-basic"
-						label="password"
-						variant="filled"
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						error={!passwordError}
-						sx={{ mt: 3, mb: 2 }}
-						helperText={
-							!passwordError
-								? 'password must only contain numbers and characters and length must be 8 or higher'
-								: ''
-						}
-					/>
-
-					<Button
-						fullWidth
-						variant="contained"
-						disabled={!emailError || !passwordError}
-						sx={{ mt: 3, mb: 2 }}>
-						Sign Up
-					</Button>
-				</Item>
-			</Grid>
-		</Grid>
-	);
+	return <div>Login Page</div>;
 };
 
 export default LoginPage;
